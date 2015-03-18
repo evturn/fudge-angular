b3b27d06d6609b14771eaa9d33ace06b8e6d20db
Evan Turner <evturn@gmail.com>
2015-03-18 00:27:34 -0400

    server setup, listening on port 8080

--- server.js
+++ server.js
@@ -3,3 +3,11 @@ var app 		= express();
 var path 		= require('path');
 
 app.use(express.static(__dirname + '/public'));
+
+// one and only route
+app.get('*', function(req, res) {
+	res.sendFile(path.join(__dirname + '/public/views/index.html'));
+});
+
+app.listen(8080);
+console.log('Express listening on 8080');
