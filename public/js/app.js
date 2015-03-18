bfff9dc1918dcc1f6c9d034031c2bf1a57790822
Evan Turner <evturn@gmail.com>
2015-03-18 00:57:06 -0400

    main and home ctrl created

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,16 @@
-angular.module('routeApp', [])
+angular.module('officalFudge', [])
 
 .controller('mainController', function() {
 
-});
+	var vm = this;
+
+	vm.bigMessage = 'Official Fudge';
+})
+
+.controller('homeController', function() {
+
+	var vm = this;
+
+	vm.message = 'I\'d like welcome you to my page, but I won\'t'
+
+})
