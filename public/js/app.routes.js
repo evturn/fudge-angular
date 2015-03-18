f22761b0e65d11208d593e16a1fd7f114b8adbe5
Evan Turner <evturn@gmail.com>
2015-03-18 01:12:24 -0400

    created for all routes

--- public/js/app.routes.js
+++ public/js/app.routes.js
@@ -1,1 +1,23 @@
 angular.module('fudgeRoutes', ['ngRoute'])
+
+.config(function($routeProvider, $locationProvider) {
+	$routeProvider
+
+		.when('/', {
+			templateUrl	 : 'views/pages/home.html',
+			controller	 : 'homeController',
+			controllerAs : 'home'
+		})
+		.when('/about', {
+			templateUrl	 : 'views/pages/about.html',
+			controller 	 : 'aboutController',
+			controllerAs : 'about'
+		})
+		.when('/contact', {
+			templateUrl	 : 'view/pages/contact.html',
+			controller 	 : 'contactController',
+			controllerAs : 'contact'
+		});
+
+
+});
