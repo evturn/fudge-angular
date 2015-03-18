86973a37eabea9951f3c0f684463730bf1d0d0fe
Evan Turner <evturn@gmail.com>
2015-03-18 00:30:29 -0400

    comments added in server file

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ var express = require('express');
 var app 		= express();
 var path 		= require('path');
 
+
+// static resources
 app.use(express.static(__dirname + '/public'));
 
-// one and only route
+// all routes and serve index
 app.get('*', function(req, res) {
 	res.sendFile(path.join(__dirname + '/public/views/index.html'));
 });
