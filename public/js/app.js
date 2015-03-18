81d8fdfb7742e86af785ddfc2502d0e1aae53b5b
Evan Turner <evturn@gmail.com>
2015-03-18 00:59:05 -0400

    contact ctrl created

--- public/js/app.js
+++ public/js/app.js
@@ -14,3 +14,19 @@ angular.module('officalFudge', [])
 	vm.message = 'I\'d like welcome you to my page, but I won\'t'
 
 })
+
+.controller('aboutController', function() {
+
+	var vm = this;
+
+	vm.message = 'A window into my life'
+
+})
+
+.controller('contactController', function() {
+
+	var vm = this;
+
+	vm.message = 'Leave me a message and I will get back to you as soon as I can. Never.'
+
+});
